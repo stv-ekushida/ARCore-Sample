b3708a3b671a45c198b15b2bf0fb5f1c54803ac8
stv-ekushida <e.kushida@st-ventures.jp>
2018-12-05 15:45:02 +0900

    Initial Commit

---
+++ ARSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARSample
+//
+//  Created by Eiji Kushida on 2018/12/05.
+//  Copyright © 2018年 Eiji Kushida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
